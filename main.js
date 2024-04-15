b6129898124f490181fa630692071e275a31a905
A Yodha Gopal <gopaal.akkineni@gmail.com>
2024-04-15 12:16:43 +0530

    perlin noise terrain

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Perlin from "./src/perlin.js";
+
 
 // camera
 const camera = new THREE.PerspectiveCamera(
   90,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  10000
 );
-camera.position.z = 5;
+camera.position.set(0,60,50)
 
 // scene
 const scene = new THREE.Scene();
@@ -17,11 +20,35 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const box = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
+var controls = new OrbitControls(camera, renderer.domElement)
+
+var geometry = new THREE.PlaneGeometry(1000, 1000, 125, 125);
+var material = new THREE.MeshLambertMaterial({wireframe: false});
+var terrain = new THREE.Mesh(geometry, material);
+terrain.rotateX(-Math.PI/2)
+scene.add(terrain)
+
+const perlin = new Perlin()
+
+const smoothing = 300;
+var peak = 60;
+var vertices = terrain.geometry.attributes.position.array
+for(var i=0;i<=vertices.length; i+=3)
+{
+  vertices[i+2] = peak * perlin.noise(
+      vertices[i]/smoothing , 
+      vertices[i+1]/smoothing
 );
-scene.add(box);
+}
+terrain.geometry.attributes.position.needsUpdate = true
+terrain.geometry.computeVertexNormals();
+
+// light
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+const directionLight = new THREE.DirectionalLight()
+directionLight.lookAt(terrain)
+scene.add(directionLight)
 
 function animate() {
   renderer.render(scene, camera);
